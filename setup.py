d9a64f879a77e7922905e6835c66e060f93d1e4c
cesteva <cesteva@gisce.net>
2026-05-28 11:23:33 +0200

    improve setup configuration

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ from setuptools import setup
 setup(
     name='codigospostales',
     version='1.0.0',
-    packages=['codigospostales'],
     description='Codigos postales',
+    packages=['codigospostales'],
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    url='https://github.com/gisce/codigospostales',
+    license='AGPL-3',
 )
